89f70d1444754962334b8c036406728d231977a8
indraarianggi <indraarianggi@gmail.com>
2018-10-02 08:00:45 +0700

    routes

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true // 404s will fallback to index.html
   }
 };
