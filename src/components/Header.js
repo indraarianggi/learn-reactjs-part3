7cc81d646a4110a47ce5d3d34b768c31cba66476
indraarianggi <indraarianggi@gmail.com>
2018-10-03 08:09:18 +0700

    query string and url parameters

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,7 @@ const Header = () => (
     <header>
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact>Home Page</NavLink> | 
-        <NavLink to="/create" activeClassName="is-active"> Create Page</NavLink> | 
-        <NavLink to="/edit" activeClassName="is-active"> Edit Page</NavLink> | 
+        <NavLink to="/create" activeClassName="is-active"> Create Page</NavLink> |  
         <NavLink to="/help" activeClassName="is-active"> Help Page</NavLink>
     </header>
 );
