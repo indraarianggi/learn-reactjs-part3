a06fc3d438fbb6eb3f13dd8ec4cc9d1a7a0de618
indraarianggi <indraarianggi@gmail.com>
2018-10-07 05:46:25 +0700

    [miss] - es6 Array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+/*
+*   Object Destructuring
+*/
+
 // const person = {
 //     // name: 'Indra Arianggi',
 //     age: 17,
@@ -21,15 +25,37 @@
 
 
 // Challenge
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-}
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// }
 
-// es6 destructuring - change variable name and set default value
-const { name: publisherName = 'Self published' } = book.publisher;
+// // es6 destructuring - change variable name and set default value
+// const { name: publisherName = 'Self published' } = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+
+
+/*
+*   Array Destructuring
+*/
+
+const address = ['Jalan Merdeka Sejati No. 17', 'Jakarta', 'Indonesia', '1945'];
+
+// const [street, city, state, zip] = address;
+const [, city, state] = address;
+// const [, city = 'Yogyakarta'] = address;
+
+console.log(`You are in ${city}, ${state}.`);
+
+
+// Challenge
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [coffee, , priceMedium] = item;
+
+console.log(`A medium ${coffee} costs ${priceMedium}`)
