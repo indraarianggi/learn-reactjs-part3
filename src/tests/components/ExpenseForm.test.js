2b1b53176ad26f3b5255cfc483f71ea075d75d95
indraarianggi <indraarianggi@gmail.com>
2018-10-27 16:36:21 +0700

    mocking libraries with jest

---
+++ src/tests/components/ExpenseForm.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import moment from 'moment';
+import ExpenseForm from '../../components/ExpenseForm';
+
+const expenses = [
+    {
+        id: '1',
+        description: 'Gum',
+        note: '',
+        amount: 195,
+        createdAt: 0
+    },
+    {
+        id: '2',
+        description: 'Rent',
+        note: '',
+        amount: 109500,
+        createdAt: moment(0).subtract(4, 'days').valueOf()
+    },
+    {
+        id: '3',
+        description: 'Credit Card',
+        note: '',
+        amount: 4500,
+        createdAt: moment(0).add(4, 'days').valueOf()
+    }
+];
+
+test('should render ExpenseForm correctly', () => {
+    const wrapper = shallow(<ExpenseForm />);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should render ExpenseForm correctly with expense data', () => {
+    const wrapper = shallow(<ExpenseForm expense={expenses[0]} />);
+    expect(wrapper).toMatchSnapshot();
+});
