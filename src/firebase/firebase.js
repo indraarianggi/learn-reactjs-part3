2586336c0d52cbb3de8d2ae2d1e79bf1cdc3b4a1
indraarianggi <indraarianggi@gmail.com>
2018-11-10 23:30:37 +0700

    firebase CRUD

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,24 +12,96 @@ const config = {
 
 firebase.initializeApp(config);
 
-// test
 const database = firebase.database();
+
+// Set (add) data to database
 database.ref().set({
     name: 'Indra Arianggi',
     age: 22,
-    isSingle: true,
+    stressLevel: 6,
+    job: {
+        title: 'Software developer',
+        company: 'Google'
+    },
     location: {
         city: 'Depok',
         country: 'Indonesia'
     }
+}).then(() => {
+    console.log('Data is saved');
+}).catch((e) => {
+    console.log('This failed. ', e);
 });
 
-// modify data
-database.ref('age').set(23);
-database.ref('location/city').set('Yogyakarta');
-
-// add new data object
-database.ref('attributes').set({
-    height: 185,
-    weight: 55
+// Update (modify) data
+database.ref().update(
+    {
+        stressLevel: 9,
+        'job/company': 'Amazon',
+        'location/city': 'Yogyakarta'
+    }
+).then(() => {
+    console.log('Data is updated');
+}).catch((e) => {
+    console.log('Update failed. ', e);
 });
+
+// Delete data
+database.ref('isSingle').remove()
+        .then(() => {
+            console.log('isSingle property has been removed');
+        }).catch((e) => {
+            console.log('Remove failed. ', e);
+        });
+
+// Get data from database (firebase)
+// Method 1
+// database.ref('job/company')
+//         .once('value')
+//         .then((snapshot) => {
+//             const val = snapshot.val();
+//             console.log(val);
+//         })
+//         .catch((e) => {
+//             console.log('Error fatching data', e);
+//         });
+
+// Method 2
+const onValueChange = database.ref()
+                            .on('value', (snapshot) => {
+                                console.log(snapshot.val());
+                            }, (e) => {
+                                console.log('Error with data fetching. ', e);
+                            });
+// setTimeout(() => {
+//     database.ref('age').set(27);
+// }, 3500);
+
+// setTimeout(() => {
+//     database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//     database.ref('age').set(30);
+// }, 10500);
+
+
+// Challenge
+// const onValueChange2 = database.ref()
+//                                 .on('value', (snapshot) => {
+//                                     const val = snapshot.val();
+//                                     const message = `${val.name} is ${val.job.title} at ${val.job.company}.`;
+//                                     console.log(message);
+//                                 }, (e) => {
+//                                     console.log('Error fetching data. ', e);
+//                                 });
+// setTimeout(() => {
+//     database.ref().update(
+//         {
+//             job: {
+//                 title: 'Fullstack Web developer',
+//                 company: 'Tokopedia'
+//             }
+//         }
+//     );
+// }, 3500);
